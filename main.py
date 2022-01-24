f7b2b4ec73cf4a55ced7cef8cb4f9f75499e48f1
darshan raul <14124900+darshan-raul@users.noreply.github.com>
2022-01-24 22:33:15 +0530

    fix: tried fixing some issues mentioned in pylint + keeping failure threshold below 5

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
+import os
 from starlette_graphene3 import GraphQLApp, make_graphiql_handler
 from fastapi import FastAPI
-from graphene.types.objecttype import ObjectType
 from graphene import ObjectType as ot
 from graphene import String as st
-from graphene import Int as int
 from graphene import List as li
 import graphene
-import logging
-
-
-import os
 
 main_file_name = os.environ["MAIN_FILE_NAME"]
 main_func_name = os.environ["MAIN_FUNCTION_NAME"]
@@ -21,14 +16,14 @@ data = [
 ]
 
 
-class students(ot):
+class Students(ot):
     name = st()
     city = st()
     country = st()
 
 
-class person(ot):
-    student = li(students)
+class Person(ot):
+    student = li(Students)
 
     def resolve_student(self, info):
         return data
@@ -36,7 +31,7 @@ class person(ot):
 
 app = FastAPI()
 
-schema = graphene.Schema(query=person)
+schema = graphene.Schema(query=Person)
 
 app.add_route("/graphql", GraphQLApp(schema, on_get=make_graphiql_handler()))
 
