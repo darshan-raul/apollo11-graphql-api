fa35d90f5091e0e7372dcda90f2ddec4ecd4a5af
darshan raul <14124900+darshan-raul@users.noreply.github.com>
2022-01-24 21:43:19 +0530

    formatted code with black and merged the workflows

--- main.py
+++ main.py
@@ -10,59 +10,54 @@ import logging
 
 
 import os
-main_file_name=os.environ['MAIN_FILE_NAME']
-main_func_name=os.environ['MAIN_FUNCTION_NAME']
 
-data=[
-    {
-        "name": "Roni",
-        "city": "Cologne",
-        "country": "India"
+main_file_name = os.environ["MAIN_FILE_NAME"]
+main_func_name = os.environ["MAIN_FUNCTION_NAME"]
 
-},
-{
-        "name": "John",
-        "city": "London",
-        "country": "UK"
-
-},
-{
-         "name": "Maria",
-         "city": "Sydney",
-         "country": "Australia"
-}
+data = [
+    {"name": "Roni", "city": "Cologne", "country": "India"},
+    {"name": "John", "city": "London", "country": "UK"},
+    {"name": "Maria", "city": "Sydney", "country": "Australia"},
 ]
 
+
 class students(ot):
-    name=st()
-    city=st()
-    country=st()
+    name = st()
+    city = st()
+    country = st()
+
 
 class person(ot):
-    student=li(students)
-    def resolve_student(self,info):
+    student = li(students)
+
+    def resolve_student(self, info):
         return data
 
-app=FastAPI()
+
+app = FastAPI()
 
 schema = graphene.Schema(query=person)
 
-app.add_route("/graphql",GraphQLApp(schema,on_get=make_graphiql_handler())) 
+app.add_route("/graphql", GraphQLApp(schema, on_get=make_graphiql_handler()))
+
 
 @app.get("/health")
 def get_health():
     return "pong"
 
+
 @app.get("/ready")
 def get_readiness_status():
     return "yes"
+
 
 @app.get("/startup")
 def get_startup_status():
     return "healthy"
 
+
 @app.get("/secret")
 def get_secret():
-    with open('/etc/secrets/fastapi-name', 'r') as file:
+    with open("/etc/secrets/fastapi-name", "r") as file:
         data = file.read().rstrip()
     return data
